29e1d30f91a96f38ae6599b59f21f5d35a4d11c5
carolinelds <carolinelds@gmail.com>
2022-01-30 03:13:39 -0300

    Enable order button (green version) when 3 foods are selected

--- scripts.js
+++ scripts.js
@@ -16,5 +16,25 @@ function selectFood(item, ident){
     // add checkmark to selected item
     const check = item.querySelector("ion-icon");
     check.classList.toggle("checkmarkOff");
+
+    checkOrderButton();
 }
 
+function checkOrderButton(){
+
+    // identify if there is one food selected in each category
+    const dishSelected = document.querySelector(".dishes .selected");
+    const drinkSelected = document.querySelector(".drinks .selected");
+    const dessertSelected = document.querySelector(".desserts .selected");
+
+    // return true or false whether there is one food selected in each category
+    let status = (dishSelected !== null) && (drinkSelected !== null) && (dessertSelected !== null);
+
+    // if true, enable order button (green version)
+    if (status) {
+        const orderButton = document.querySelector(".order-button");
+
+        orderButton.classList.add("enabled");
+        orderButton.innerHTML = "Fechar pedido";
+    }
+}
